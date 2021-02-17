fe555c7c0f98219f9bc8851f0cee5251caa4f469
wsy8029 <wsy8029@gmail.com>
2021-02-17 14:29:33 +0900

    test

--- src/main.py
+++ src/main.py
@@ -34,3 +34,16 @@ if __name__ == '__main__':
     user_id = ''
     rec_list = recommend_content(user_id)
     print(rec_list)
+
+    from surprise import SVD
+    from surprise import Dataset
+    from surprise.model_selection import cross_validate
+
+    # Load the movielens-100k dataset (download it if needed),
+    data = Dataset.load_builtin('ml-100k')
+
+    # We'll use the famous SVD algorithm.
+    algo = SVD()
+
+    # Run 5-fold cross-validation and print results
+    cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
